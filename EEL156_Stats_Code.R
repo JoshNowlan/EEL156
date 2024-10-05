b686ff781243d00b88815e6ba047f44307bc3b73
JoshNowlan <93714853+JoshNowlan@users.noreply.github.com>
2024-10-05 14:45:30 -0400

    Update EEL156_Stats_Code.R
    Added variables. Streamlined emmeans.

--- EEL156_Stats_Code.R
+++ EEL156_Stats_Code.R
@@ -521,64 +521,201 @@ emmeans(CadencexConditionxDistance.model, list(pairwise ~ Condition), at = list(
 emmeans(CadencexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "15km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline
 emmeans(CadencexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "20km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-#Variable 4 - Heat_Exchange
 
-# Step 1 - visualize Heat_Exchange data. This is a good way to visualize and identify outliers
-boxplot(import_data4$Heat_Exchange ~ import_data4$Condition)
-boxplot(import_data4$Heat_Exchange ~ import_data4$Distance)
+# Variable 5 - Finger_Temp
 
-# Next step, need to first build the model, then check for normal distribution and homoscedascticity. If all good, run the ANOVA, if significant (p < 0.05) run pairwise comparisons
+# Step 1 - Visualize Finger_Temp data
+boxplot(import_data4$Finger_Temp ~ import_data4$Condition)
+boxplot(import_data4$Finger_Temp ~ import_data4$Distance)
 
-# Step 2 - create model Heat_Exchange x Condition
-Heat_ExchangexConditionxDistance.model <- lmer(Heat_Exchange ~ Condition * Distance + (1|Participant), data = import_data4) # Builds the model w/ Random intercept for participant
+# Step 2 - Create model Finger_Temp x Condition x Distance
+Finger_TempxConditionxDistance.model <- lmer(Finger_Temp ~ Condition * Distance + (1|Participant), data = import_data4)
 
-# Step 3 Check for normality using wilk shapiro (only works if no missing data) and Q-Q Plot
+# Step 3 - Check for normality using Shapiro-Wilk and Q-Q Plot
+hist(residuals(Finger_TempxConditionxDistance.model), freq = FALSE, main = "Residuals Histogram")
+residuals_Finger_Temp <- residuals(Finger_TempxConditionxDistance.model)
+qqnorm(residuals_Finger_Temp)
+qqline(residuals_Finger_Temp)
+shapiro.test(residuals_Finger_Temp)
 
-hist(residuals(Heat_ExchangexConditionxDistance.model), freq = FALSE, main = "Residuals Histogram")
-residuals_Heat_Exchange <- residuals(Heat_ExchangexConditionxDistance.model) #determine residuals for levines test
-qqnorm(residuals_Heat_Exchange) #QQ Plot for Normality
-qqline(residuals_Heat_Exchange) #QQ Plot for Normality
-shapiro.test(residuals_Heat_Exchange) #(if p < 0.05, no normality combined with Q-Q plot interpretation)
+# Step 4 - Check for homoscedasticity using a residuals plot and Levene's test
+plot(Finger_TempxConditionxDistance.model, resid = TRUE)
+leveneTest(Finger_Temp ~ Condition, data = import_data4)
 
-#Step 4 Check for homoscedasticity using a plot and levenetest (if all data present)
+# Step 5 - Find Means and Standard Deviations
+mean_Finger_Temp <- aggregate(Finger_Temp ~ Condition + Distance, data = import_data4, FUN = mean, na.rm = TRUE)
+sd_Finger_Temp <- aggregate(Finger_Temp ~ Condition + Distance, data = import_data4, FUN = sd, na.rm = TRUE)
+summary_stats_Finger_Temp <- merge(mean_Finger_Temp, sd_Finger_Temp, by = c("Condition", "Distance"))
+names(summary_stats_Finger_Temp) <- c("Condition", "Distance", "Mean_Finger_Temp", "SD_Finger_Temp")
+summary_stats_Finger_Temp$Condition <- factor(summary_stats_Finger_Temp$Condition,
+                                              levels = c("10C", "20C", "32C"))
+summary_stats_Finger_Temp$Distance <- factor(summary_stats_Finger_Temp$Distance,
+                                             levels = c("0km", "5km", "10km", "15km", "20km"))
+summary_stats_Finger_Temp <- summary_stats_Finger_Temp[order(summary_stats_Finger_Temp$Condition, summary_stats_Finger_Temp$Distance), ]
+summary_stats_Finger_Temp$Mean_Finger_Temp <- round(summary_stats_Finger_Temp$Mean_Finger_Temp, 0)
+summary_stats_Finger_Temp$SD_Finger_Temp <- round(summary_stats_Finger_Temp$SD_Finger_Temp, 0)
+print(summary_stats_Finger_Temp)
 
-plot(Heat_ExchangexConditionxDistance.model, resid = TRUE) #visual plot of residuals over fitted model, aiming for constant spread around zero, do not want funnel shape or systematice change in spread
-leveneTest(Heat_Exchange ~ Condition, data = import_data4) #(if p < 0.05, no homoscedasticity combined with plot interpretation)
+# Step 6 - RM-ANOVA and effect size
+anova(Finger_TempxConditionxDistance.model)
+effectsize::eta_squared(Finger_TempxConditionxDistance.model, partial = TRUE)
 
-#Step 5 Find Means and Standard Deviations to be used in results section and graphpad figures:
+# Step 7 - Pairwise comparisons for Condition
+emmeans(Finger_TempxConditionxDistance.model, list(pairwise ~ Condition), adjust = "bonferroni")
 
-mean_Heat_Exchange <- aggregate(Heat_Exchange ~ Condition + Distance, data = import_data4, FUN = mean, na.rm = TRUE)
-sd_Heat_Exchange <- aggregate(Heat_Exchange ~ Condition + Distance, data = import_data4, FUN = sd, na.rm = TRUE)
-summary_stats_Heat_Exchange <- merge(mean_Heat_Exchange, sd_Heat_Exchange, by = c("Condition", "Distance"))
-names(summary_stats_Heat_Exchange) <- c("Condition", "Distance", "Mean_Heat_Exchange", "SD_Heat_Exchange")
-summary_stats_Heat_Exchange$Condition <- factor(summary_stats_Heat_Exchange$Condition,
-                                          levels = c("10C", "20C", "32C"))
+# Step 8 - Pairwise comparisons for Distance
+emmeans(Finger_TempxConditionxDistance.model, list(pairwise ~ Distance), adjust = "bonferroni")
 
-summary_stats_Heat_Exchange$Distance <- factor(summary_stats_Heat_Exchange$Distance,
-                                         levels = c("0km", "5km", "10km", "15km", "20km"))
-summary_stats_Heat_Exchange <- summary_stats_Heat_Exchange[order(summary_stats_Heat_Exchange$Condition, summary_stats_Heat_Exchange$Distance), ]
-summary_stats_Heat_Exchange$Mean_Heat_Exchange <- round(summary_stats_Heat_Exchange$Mean_Heat_Exchange, 0) #Change the number at the end for number of decimal places
-summary_stats_Heat_Exchange$SD_Heat_Exchange <- round(summary_stats_Heat_Exchange$SD_Heat_Exchange, 0)  #Change the number at the end for number of decimal places
-print(summary_stats_Heat_Exchange)
+# Step 9 - Pairwise comparisons for Condition*Distance Interaction
+distances <- c("0km", "5km", "10km", "15km", "20km")
+for (distance in distances) {
+  emmeans(Finger_TempxConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = distance), adjust = "bonferroni")
+}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Variable 5 - Mean_Skin_Temp
 
+# Step 1 - Visualize Mean_Skin_Temp data
+boxplot(import_data4$Mean_Skin_Temp ~ import_data4$Condition)
+boxplot(import_data4$Mean_Skin_Temp ~ import_data4$Distance)
 
-# Step 6 Run an RM-ANOVA on the LMM to get p values and effect size
+# Step 2 - Create model Mean_Skin_Temp x Condition x Distance
+Mean_Skin_TempxConditionxDistance.model <- lmer(Mean_Skin_Temp ~ Condition * Distance + (1|Participant), data = import_data4)
 
-anova(Heat_ExchangexConditionxDistance.model) # output typical ANOVA stats
-effectsize::eta_squared(Heat_ExchangexConditionxDistance.model, partial = TRUE) # calclate partial eta square effect size
+# Step 3 - Check for normality using Shapiro-Wilk and Q-Q Plot
+hist(residuals(Mean_Skin_TempxConditionxDistance.model), freq = FALSE, main = "Residuals Histogram")
+residuals_Mean_Skin_Temp <- residuals(Mean_Skin_TempxConditionxDistance.model)
+qqnorm(residuals_Mean_Skin_Temp)
+qqline(residuals_Mean_Skin_Temp)
+shapiro.test(residuals_Mean_Skin_Temp)
 
-#Step 7 If Condition is significant (p < 0.05), run pairwise comparisons, if not, do not run
+# Step 4 - Check for homoscedasticity using a residuals plot and Levene's test
+plot(Mean_Skin_TempxConditionxDistance.model, resid = TRUE)
+leveneTest(Mean_Skin_Temp ~ Condition, data = import_data4)
 
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Condition), adjust = "bonferroni") # pairwise comp w/ Bonferroni
+# Step 5 - Find Means and Standard Deviations
+mean_Mean_Skin_Temp <- aggregate(Mean_Skin_Temp ~ Condition + Distance, data = import_data4, FUN = mean, na.rm = TRUE)
+sd_Mean_Skin_Temp <- aggregate(Mean_Skin_Temp ~ Condition + Distance, data = import_data4, FUN = sd, na.rm = TRUE)
+summary_stats_Mean_Skin_Temp <- merge(mean_Mean_Skin_Temp, sd_Mean_Skin_Temp, by = c("Condition", "Distance"))
+names(summary_stats_Mean_Skin_Temp) <- c("Condition", "Distance", "Mean_Mean_Skin_Temp", "SD_Mean_Skin_Temp")
+summary_stats_Mean_Skin_Temp$Condition <- factor(summary_stats_Mean_Skin_Temp$Condition,
+                                                 levels = c("10C", "20C", "32C"))
+summary_stats_Mean_Skin_Temp$Distance <- factor(summary_stats_Mean_Skin_Temp$Distance,
+                                                levels = c("0km", "5km", "10km", "15km", "20km"))
+summary_stats_Mean_Skin_Temp <- summary_stats_Mean_Skin_Temp[order(summary_stats_Mean_Skin_Temp$Condition, summary_stats_Mean_Skin_Temp$Distance), ]
+summary_stats_Mean_Skin_Temp$Mean_Mean_Skin_Temp <- round(summary_stats_Mean_Skin_Temp$Mean_Mean_Skin_Temp, 0)
+summary_stats_Mean_Skin_Temp$SD_Mean_Skin_Temp <- round(summary_stats_Mean_Skin_Temp$SD_Mean_Skin_Temp, 0)
+print(summary_stats_Mean_Skin_Temp)
 
-#Step 8 If Distance is significant (p < 0.05), run pairwise comparisons, if not, do not run
+# Step 6 - RM-ANOVA and effect size
+anova(Mean_Skin_TempxConditionxDistance.model)
+effectsize::eta_squared(Mean_Skin_TempxConditionxDistance.model, partial = TRUE)
 
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Distance), adjust = "bonferroni") # pairwise comp w/ Bonferroni
+# Step 7 - Pairwise comparisons for Condition
+emmeans(Mean_Skin_TempxConditionxDistance.model, list(pairwise ~ Condition), adjust = "bonferroni")
 
-#Step 9 If Condition* Distance Interaction is significant (p < 0.05), run pairwise comparisons, if not, do not run. Here, you are essentially running an RM ANOVA at each distance
+# Step 8 - Pairwise comparisons for Distance
+emmeans(Mean_Skin_TempxConditionxDistance.model, list(pairwise ~ Distance), adjust = "bonferroni")
 
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "0km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "5km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "10km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "15km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline
-emmeans(Heat_ExchangexConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = "20km"), adjust = "bonferroni") # pairwise comp for interaction w/ Bonferroni at baseline#Variable 4 - Cadence
+# Step 9 - Pairwise comparisons for Condition*Distance Interaction
+for (distance in distances) {
+  emmeans(Mean_Skin_TempxConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = distance), adjust = "bonferroni")
+}
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Variable 5 - Cleaned_HR
+
+# Step 1 - Visualize Cleaned_HR data
+boxplot(import_data4$Cleaned_HR ~ import_data4$Condition)
+boxplot(import_data4$Cleaned_HR ~ import_data4$Distance)
+
+# Step 2 - Create model Cleaned_HR x Condition x Distance
+Cleaned_HRxConditionxDistance.model <- lmer(Cleaned_HR ~ Condition * Distance + (1|Participant), data = import_data4)
+
+# Step 3 - Check for normality using Shapiro-Wilk and Q-Q Plot
+hist(residuals(Cleaned_HRxConditionxDistance.model), freq = FALSE, main = "Residuals Histogram")
+residuals_Cleaned_HR <- residuals(Cleaned_HRxConditionxDistance.model)
+qqnorm(residuals_Cleaned_HR)
+qqline(residuals_Cleaned_HR)
+shapiro.test(residuals_Cleaned_HR)
+
+# Step 4 - Check for homoscedasticity using a residuals plot and Levene's test
+plot(Cleaned_HRxConditionxDistance.model, resid = TRUE)
+leveneTest(Cleaned_HR ~ Condition, data = import_data4)
+
+# Step 5 - Find Means and Standard Deviations
+mean_Cleaned_HR <- aggregate(Cleaned_HR ~ Condition + Distance, data = import_data4, FUN = mean, na.rm = TRUE)
+sd_Cleaned_HR <- aggregate(Cleaned_HR ~ Condition + Distance, data = import_data4, FUN = sd, na.rm = TRUE)
+summary_stats_Cleaned_HR <- merge(mean_Cleaned_HR, sd_Cleaned_HR, by = c("Condition", "Distance"))
+names(summary_stats_Cleaned_HR) <- c("Condition", "Distance", "Mean_Cleaned_HR", "SD_Cleaned_HR")
+summary_stats_Cleaned_HR$Condition <- factor(summary_stats_Cleaned_HR$Condition,
+                                             levels = c("10C", "20C", "32C"))
+summary_stats_Cleaned_HR$Distance <- factor(summary_stats_Cleaned_HR$Distance,
+                                            levels = c("0km", "5km", "10km", "15km", "20km"))
+summary_stats_Cleaned_HR <- summary_stats_Cleaned_HR[order(summary_stats_Cleaned_HR$Condition, summary_stats_Cleaned_HR$Distance), ]
+summary_stats_Cleaned_HR$Mean_Cleaned_HR <- round(summary_stats_Cleaned_HR$Mean_Cleaned_HR, 0)
+summary_stats_Cleaned_HR$SD_Cleaned_HR <- round(summary_stats_Cleaned_HR$SD_Cleaned_HR, 0)
+print(summary_stats_Cleaned_HR)
+
+# Step 6 - RM-ANOVA and effect size
+anova(Cleaned_HRxConditionxDistance.model)
+effectsize::eta_squared(Cleaned_HRxConditionxDistance.model, partial = TRUE)
+
+# Step 7 - Pairwise comparisons for Condition
+emmeans(Cleaned_HRxConditionxDistance.model, list(pairwise ~ Condition), adjust = "bonferroni")
+
+# Step 8 - Pairwise comparisons for Distance
+emmeans(Cleaned_HRxConditionxDistance.model, list(pairwise ~ Distance), adjust = "bonferroni")
+
+# Step 9 - Pairwise comparisons for Condition*Distance Interaction
+for (distance in distances) {
+  emmeans(Cleaned_HRxConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = distance), adjust = "bonferroni")
+}
+###########################################################################################
+# Variable 5 - Immersion_Box_Temp
+
+# Step 1 - Visualize Immersion_Box_Temp data
+boxplot(import_data4$Immersion_Box_Temp ~ import_data4$Condition)
+boxplot(import_data4$Immersion_Box_Temp ~ import_data4$Distance)
+
+# Step 2 - Create model Immersion_Box_Temp x Condition x Distance
+Immersion_Box_TempxConditionxDistance.model <- lmer(Immersion_Box_Temp ~ Condition * Distance + (1|Participant), data = import_data4)
+
+# Step 3 - Check for normality using Shapiro-Wilk and Q-Q Plot
+hist(residuals(Immersion_Box_TempxConditionxDistance.model), freq = FALSE, main = "Residuals Histogram")
+residuals_Immersion_Box_Temp <- residuals(Immersion_Box_TempxConditionxDistance.model)
+qqnorm(residuals_Immersion_Box_Temp)
+qqline(residuals_Immersion_Box_Temp)
+shapiro.test(residuals_Immersion_Box_Temp)
+
+# Step 4 - Check for homoscedasticity using a residuals plot and Levene's test
+plot(Immersion_Box_TempxConditionxDistance.model, resid = TRUE)
+leveneTest(Immersion_Box_Temp ~ Condition, data = import_data4)
+
+# Step 5 - Find Means and Standard Deviations
+mean_Immersion_Box_Temp <- aggregate(Immersion_Box_Temp ~ Condition + Distance, data = import_data4, FUN = mean, na.rm = TRUE)
+sd_Immersion_Box_Temp <- aggregate(Immersion_Box_Temp ~ Condition + Distance, data = import_data4, FUN = sd, na.rm = TRUE)
+summary_stats_Immersion_Box_Temp <- merge(mean_Immersion_Box_Temp, sd_Immersion_Box_Temp, by = c("Condition", "Distance"))
+names(summary_stats_Immersion_Box_Temp) <- c("Condition", "Distance", "Mean_Immersion_Box_Temp", "SD_Immersion_Box_Temp")
+summary_stats_Immersion_Box_Temp$Condition <- factor(summary_stats_Immersion_Box_Temp$Condition,
+                                                     levels = c("10C", "20C", "32C"))
+summary_stats_Immersion_Box_Temp$Distance <- factor(summary_stats_Immersion_Box_Temp$Distance,
+                                                    levels = c("0km", "5km", "10km", "15km", "20km"))
+summary_stats_Immersion_Box_Temp <- summary_stats_Immersion_Box_Temp[order(summary_stats_Immersion_Box_Temp$Condition, summary_stats_Immersion_Box_Temp$Distance), ]
+summary_stats_Immersion_Box_Temp$Mean_Immersion_Box_Temp <- round(summary_stats_Immersion_Box_Temp$Mean_Immersion_Box_Temp, 0)
+summary_stats_Immersion_Box_Temp$SD_Immersion_Box_Temp <- round(summary_stats_Immersion_Box_Temp$SD_Immersion_Box_Temp, 0)
+print(summary_stats_Immersion_Box_Temp)
+
+# Step 6 - RM-ANOVA and effect size
+anova(Immersion_Box_TempxConditionxDistance.model)
+effectsize::eta_squared(Immersion_Box_TempxConditionxDistance.model, partial = TRUE)
+
+# Step 7 - Pairwise comparisons for Condition
+emmeans(Immersion_Box_TempxConditionxDistance.model, list(pairwise ~ Condition), adjust = "bonferroni")
+
+# Step 8 - Pairwise comparisons for Distance
+emmeans(Immersion_Box_TempxConditionxDistance.model, list(pairwise ~ Distance), adjust = "bonferroni")
+
+# Step 9 - Pairwise comparisons for Condition*Distance Interaction
+for (distance in distances) {
+  emmeans(Immersion_Box_TempxConditionxDistance.model, list(pairwise ~ Condition), at = list(Time = distance), adjust = "bonferroni")
+}
+
